2c0c88312fdf7ffb7ec1524c397e062c40e2bad3
Prashant tomar <143907351+Thakurprashan@users.noreply.github.com>
2025-04-10 23:32:44 +0530

    Add files via upload

---
+++ Day 2/script.js
@@ -0,0 +1,68 @@
+console.log("JS DAY 2");
+// Increment and Decrement Operator
+
+let a1=20;
+console.log(++a1, a1, --a1,a1--,a1--,a1,a1,a1--,a1++,++a1,++a1,a1++,--a1);
+
+let a2=20;
+console.log(a2--, a2, --a2, --a2,  a2--,a2, a2, a2, --a2, a2--, a2--);
+
+let a3=20;
+console.log(a3++,a3);
+
+let a4=20;
+console.log(a4--,a4);
+
+let a5=20;
+console.log(++a5,a5);
+
+let a6=20;
+console.log(++a6, a6, a6++, ++a6, a6, a6++, a6++, ++a6,++a6);
+
+let a7=20;
+console.log(--a7,--a7,a7,a7++,++a7);
+
+let a9=20;
+console.log(--a9,++a9,a9--,a9,a9++,a9--,--a9);
+
+
+// Logical Operators
+
+let  x=15;
+let  y=20;
+console.log(x>y && y>x);   // output false
+
+
+let  c=20;
+let  d=20;
+console.log(c===d && d==c);  // output true
+
+
+let e=20;
+let f="20";
+console.log(e!==f && f==e);   // output true
+ 
+
+let g=20;
+let h="20";
+console.log(g===g || h==g); // output true
+
+let i=50;
+let j=30;
+console.log(i<j || j>i); // output false
+
+let k=40;
+let l=40;
+console.log(k!==l);
+
+let m=40;
+let n="40";
+console.log(m!==n);
+
+let o=40;
+let p="40";
+console.log(o===p);
+
+
+
+
