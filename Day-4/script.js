5eff52584bdede67198b215deb9e1df227f5a854
Prashant Tomar <143907351+Thakurprashan@users.noreply.github.com>
2025-04-12 23:43:17 +0530

    Update script.js

--- Day-4/script.js
+++ Day-4/script.js
@@ -1,1 +1,10 @@
-
+                                                          // for loop
+for(let i=1; i<=5;i++){
+    console.log("my name is navya singh");
+}
+                                                          // while Loop
+j=0;
+while(j<10){
+    console.log("the number is",j);
+    j++;
+}
