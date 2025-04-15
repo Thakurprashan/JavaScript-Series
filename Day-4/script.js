7300f5d2750e3cc2c18f1bb184bf50ab3f6736aa
Prashant Tomar <143907351+Thakurprashan@users.noreply.github.com>
2025-04-15 23:56:31 +0530

    Update script.js

--- Day-4/script.js
+++ Day-4/script.js
@@ -18,7 +18,7 @@ let rows = 5;
 
 let pattern = "";
 
-// outer loop runs for `rows` no. of times
+
 for (let n = 1; n <= rows; n++) {
    // Inner loop - I -> prints spaces
    for (let space = 1; space <= rows - n; space++) {
