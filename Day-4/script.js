57465860b8f23b56b728e20181151fe468952be5
Prashant Tomar <143907351+Thakurprashan@users.noreply.github.com>
2025-04-15 23:55:57 +0530

    Update script.js

--- Day-4/script.js
+++ Day-4/script.js
@@ -12,7 +12,37 @@ for (let m=1; m<=5; m++){
 }
     console.log(pattern);
 }
-                                                          //  for-in loop
+
+
+let rows = 5;
+
+// pattern variable carries the final pattern in string format
+let pattern = "";
+
+// outer loop runs for `rows` no. of times
+for (let n = 1; n <= rows; n++) {
+   // Inner loop - I -> prints spaces
+   for (let space = 1; space <= rows - n; space++) {
+      pattern += " ";
+   }
+
+   // Inner Loop - II -> prints stars
+   for (let num = 1; num <= n; num++) {
+      pattern += "*";
+   }
+   pattern += "\n";
+}
+console.log(pattern);
+
+
+
+
+
+
+
+
+
+//  for-in loop
 
 
 
